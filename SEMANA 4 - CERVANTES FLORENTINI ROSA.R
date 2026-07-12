ae8e86aedf6b452b170fac19353c4cdd1d7caefb
Rosa Cervantes Florentini <933450122rosa@gmail.com>
2026-07-12 03:50:48 -0500

    Add files via upload

---
+++ SEMANA 4 - CERVANTES FLORENTINI ROSA.R
@@ -0,0 +1,15 @@
+#INSTALACIÓN DE PAQUETES
+install.packages("shiny")
+library(shiny)
+
+install.packages("readr")
+library(readr)
+
+install.packages("tidyr")
+library(tidyr)
+
+install.packages("dplyr")
+library(dplyr)
+
+install.packages("ggplot2")
+library(ggplot2)
